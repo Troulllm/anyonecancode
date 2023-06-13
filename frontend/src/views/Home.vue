4bcb504d6b718bc8a402194982a62a7d4b2e794f
Troulllm <136508965+Troulllm@users.noreply.github.com>
2023-06-13 20:06:33 +0000

    updated title

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,8 +6,8 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2" style="color: green">
-          Sophiagram
+        <b-navbar-item id="fontnavbar" class="title is-2" style="color: purple">
+          Linda is learning with Megan
         </b-navbar-item>
       </template>
     </b-navbar>
